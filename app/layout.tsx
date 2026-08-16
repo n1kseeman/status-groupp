acaca22a9bd56eaf2d271e95dd4c26580d5c371d
n1kseeman <270120225+n1kseeman@users.noreply.github.com>
2026-08-16 19:21:21 +0300

    Make the visual refresh unmistakable

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,7 +61,7 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
-  themeColor: "#f7efdc",
+  themeColor: "#183729",
 };
 
 function Wordmark() {
